539a51e4eabb09781cab472f7e51583955d2eef1
Petr Antonov <antpeter@gmail.com>
2017-06-29 18:15:32 +0300

    Reverse order of posts.

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         'published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts[::-1]})
 
 
 def post_detail(request, pk):
